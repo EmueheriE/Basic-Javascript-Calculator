8d9e33e59ce609f4932fd9294250e1cf73692873
Lovetemi <105287941+EmueheriE@users.noreply.github.com>
2022-06-24 16:11:56 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var num1 = window.prompt("Enter a number");
+var num2 = window.prompt("Enter another number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+alert(num1+num2);
+
+var num3 = window.prompt("Enter a number");
+var num4 = window.prompt("Enter another number");
+
+num1 = parseFloat(num3);
+num2 = parseFloat(num4);
+
+alert(num3-num4);
+
+var num5 = window.prompt("Enter a number");
+var num6 = window.prompt("Enter another number");
+
+num5 = parseFloat(num5);
+num6 = parseFloat(num6);
+
+alert(num5/num6);
